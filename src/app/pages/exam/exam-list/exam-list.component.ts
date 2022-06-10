56acee1131b5d178047715ee477746171e508d8a
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2022-06-11 01:14:11 +0200

    Few styles and mock levels

--- src/app/pages/exam/exam-list/exam-list.component.ts
+++ src/app/pages/exam/exam-list/exam-list.component.ts
@@ -9,6 +9,8 @@ import { ExamService } from 'src/app/services/exam.service';
 })
 export class ExamListComponent implements OnInit {
 
+  LEVELS: string[] = ["A1", "A2", "B1", "B2", "C1", "C1 HS", "C2"];
+
   exams: ExamModel[] = [];
 
   constructor(private examService: ExamService) { }
